0464956c2d380b08f4675da1b574b8f74b9069ac
ilefM <ilefikhelefdev1@gmail.com>
2023-05-11 14:40:57 -0400

    fix prettier issues

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -3,23 +3,23 @@ import { SlLogin } from "react-icons/sl";
 import { Link } from "react-router-dom";
 
 export default function MobileNavbar() {
-	return (
-		<div className="h-14 flex items-center justify-evenly  bg-black-secondary">
-			<div className="h-30 w-30">
-				<Link to="/">
-					<BiHomeAlt2 size="20px" />
-				</Link>
-			</div>
-			<div className="h-30 w-30 flex items-center">
-				<button>
-					<BiSearch size="20px" />
-				</button>
-			</div>
-			<div className="h-30 w-30">
-				<Link to="/login">
-					<SlLogin size="20px" />
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-14 flex items-center justify-evenly  bg-black-secondary">
+      <div className="h-30 w-30">
+        <Link to="/">
+          <BiHomeAlt2 size="20px" />
+        </Link>
+      </div>
+      <div className="h-30 w-30 flex items-center">
+        <button>
+          <BiSearch size="20px" />
+        </button>
+      </div>
+      <div className="h-30 w-30">
+        <Link to="/login">
+          <SlLogin size="20px" />
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,14 +1,14 @@
 import { BiSearch } from "react-icons/bi";
 
 export default function Searchbar() {
-	return (
-		<div className="flex items-center justify-between bg-gray-600 h-10 w-full p-2 rounded-lg cursor-text">
-			<input
-				type="text"
-				placeholder="Search"
-				className="text-gray-400 focus:text-white bg-transparent outline-none w-full"
-			/>
-			<BiSearch size="22px" className="ml-1" />
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-between bg-gray-600 h-10 w-full p-2 rounded-lg cursor-text">
+      <input
+        type="text"
+        placeholder="Search"
+        className="text-gray-400 focus:text-white bg-transparent outline-none w-full"
+      />
+      <BiSearch size="22px" className="ml-1" />
+    </div>
+  );
 }
