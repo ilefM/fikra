7c60def303b6dd6da1681a817d004743de9dffa3
ilefM <ilefikhelefdev1@gmail.com>
2023-05-22 21:40:10 -0400

    implementing theme switcher

--- src/components/navBar/ThemeSwitcher.tsx
+++ src/components/navBar/ThemeSwitcher.tsx
@@ -6,16 +6,16 @@ export default function ThemeSwitcher() {
   const [darkMode, setDarkMode] = useState(true);
 
   function changeTheme() {
-    if (darkMode) {
-      console.log("switched to light mode");
-      setDarkMode(false);
-    } else {
-      console.log("switched to dark mode");
-      setDarkMode(true);
-    }
+    const bodyClass = document.body.classList;
+    console.log(bodyClass);
+    darkMode ? bodyClass.remove("dark") : bodyClass.add("dark");
+    setDarkMode(!darkMode);
   }
   return (
-    <span className="sm:ml-5" onClick={changeTheme}>
+    <span
+      className="ease-in-out; cursor-pointer transition duration-300 sm:ml-5"
+      onClick={changeTheme}
+    >
       {darkMode ? <FaSun size="25" /> : <BsFillMoonFill size="25" />}
     </span>
   );
