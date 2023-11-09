db7e55c26aa745a16705e4bf8a59bbd6278cc854
ilefM <ilefikhelefdev1@gmail.com>
2023-11-08 22:47:16 -0500

    post details page

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -1,17 +1,28 @@
 function PostDetails() {
   return (
-    <div className="flex flex-col sm:flex-row w-7/12 justify-around ">
-      <div className="mb-6 w-full sm:w-[500px] rounded-lg bg-dark-200 p-5 shadow-md">
-        <textarea className="min-h-[100px] w-full resize-none break-words bg-transparent outline-none"></textarea>
+    <div className="flex flex-col sm:flex-row w-full sm:max-w-[900px] justify-around">
+      <div className="mb-6 sm:mb-0 w-full sm:w-[300px] rounded-lg bg-dark-200 p-5 shadow-md">
+        <textarea className="resize-none break-words bg-transparent outline-none"></textarea>
       </div>
-      <div className="flex flex-col items-start">
+      <div className="border flex flex-col sm:w-2/4 items-start">
         <div className="flex flex-col items-start mb-4">
-          <p>Author: </p>
-          <p>Published at:</p>
-          <p>ID:</p>
+          <div className="w-full flex justify-between">
+            <p>Author: </p>
+            <p>binary_dev</p>
+          </div>
+          <div className="w-full flex justify-between">
+            <p>Published at:</p>
+            <p>2023-01-01</p>
+          </div>
+          <div className="w-full flex justify-between">
+            <p>ID:</p>
+            <p>952fd418-54d1-4530-a397-a34dbaa07f04</p>
+          </div>
         </div>
         <div className="flex w-full justify-around">
-          <button className=" border py-2 px-5 rounded-lg">Edit</button>
+          <button className=" border py-1 px-4 sm:py-2 sm:px-5 rounded-lg">
+            Edit
+          </button>
           <button>Delete</button>
         </div>
       </div>
