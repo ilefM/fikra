da25aa20361bec4631bc7ee03a8372c6c325f4aa
ilefM <ilefikhelefdev1@gmail.com>
2023-11-10 15:58:51 -0500

    finishing the post details page

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -1,29 +1,31 @@
 function PostDetails() {
   return (
-    <div className="flex flex-col sm:flex-row w-full sm:max-w-[900px] justify-around">
-      <div className="mb-6 sm:mb-0 w-full sm:w-[300px] rounded-lg bg-dark-200 p-5 shadow-md">
-        <textarea className="resize-none break-words bg-transparent outline-none"></textarea>
+    <div className="flex flex-col sm:flex-row w-full sm:max-w-[900px] sm:justify-around">
+      <div className="mb-6 sm:mb-0 rounded-lg sm:max-w-[300px] md:w-[300px] bg-dark-200 p-3 shadow-md">
+        <textarea className="w-full h-full resize-none break-words bg-transparent outline-none"></textarea>
       </div>
-      <div className="border flex flex-col sm:w-2/4 items-start">
-        <div className="flex flex-col items-start mb-4">
-          <div className="w-full flex justify-between">
+      <div className="flex flex-col sm:w-[300px] items-start">
+        <div className="w-full flex flex-col items-start mb-5">
+          <div className="w-full mb-2">
             <p>Author: </p>
             <p>binary_dev</p>
           </div>
-          <div className="w-full flex justify-between">
+          <div className="w-full mb-2">
             <p>Published at:</p>
             <p>2023-01-01</p>
           </div>
-          <div className="w-full flex justify-between">
+          <div className="w-full mb-2">
             <p>ID:</p>
             <p>952fd418-54d1-4530-a397-a34dbaa07f04</p>
           </div>
         </div>
-        <div className="flex w-full justify-around">
-          <button className=" border py-1 px-4 sm:py-2 sm:px-5 rounded-lg">
+        <div className="flex w-full justify-around sm:justify-between">
+          <button className="border-2 border-dark-0 py-1 px-3 rounded-md">
             Edit
           </button>
-          <button>Delete</button>
+          <button className="border-2 border-red-custom py-1 px-3 rounded-md">
+            Delete
+          </button>
         </div>
       </div>
     </div>
