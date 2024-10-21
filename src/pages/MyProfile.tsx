1214405741efc249130c1e995cc30b26a9abbacc
ilefM <ilefikhelefdev1@gmail.com>
2024-10-20 23:42:46 -0400

    fix linter

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -2,16 +2,41 @@ import { useEffect, useState } from "react";
 import useGetPosts from "../hooks/posts/useGetPosts";
 import { IPost } from "../interfaces/IPost";
 import PostsList from "../components/PostsList";
+import useLoadingModal from "../hooks/loadingModal/useLoadingModal";
+import { Store } from "react-notifications-component";
 
 export default function MyProfile() {
   const [posts, setPosts] = useState<IPost[]>([]);
   const { data, fetchError, isLoading } = useGetPosts();
+  const { openModal, closeModal } = useLoadingModal();
 
   useEffect(() => {
+    if (isLoading) {
+      openModal();
+    } else {
+      closeModal();
+    }
+
+    if (fetchError) {
+      Store.addNotification({
+        title: "Error!",
+        message:
+          "We encountered an error while processing your action: " + fetchError,
+        type: "danger",
+        insert: "bottom",
+        container: "bottom-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3000,
+          onScreen: true,
+        },
+      });
+    }
     if (data) {
       setPosts(data);
     }
-  }, [data]);
+  }, [data, fetchError, isLoading, openModal, closeModal]);
 
   return (
     <div className="flex flex-col items-center max-w-[600px] w-full">
