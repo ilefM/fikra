a34afe1d6c05ee7b2fc92f6d4b7bd4ac48e2213e
ilefM <ilefikhelefdev1@gmail.com>
2024-10-20 23:13:13 -0400

    mergin

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -2,21 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { IPostDetails } from "../interfaces/IPost";
 import { useNavigate, useParams } from "react-router-dom";
 import useGetPost from "../hooks/posts/useGetPost";
-import IsLoading from "../components/LoadingModal";
-import Error from "../components/Error";
 import { ConvertDateFormat } from "../utils/dateConverter";
 import { deletePost, updatePost } from "../api/postsApi";
-import { getErrorMessage } from "../hooks/error";
+import useAuth from "../hooks/auth/useAuth";
+import { Store } from "react-notifications-component";
+import useLoadingModal from "../hooks/loadingModal/useLoadingModal";
 
 export default function PostDetails() {
   const { id } = useParams();
   const { data, fetchError, isLoading } = useGetPost(id ? id : "");
-  const [error, setError] = useState("");
-  const [isProcessing, setIsProcessing] = useState(false);
   const [post, setPost] = useState<IPostDetails>();
   const [postUpdated, setPostUpdated] = useState<IPostDetails>();
   const navigate = useNavigate();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const { isAuthenticated } = useAuth();
+  const { openModal, closeModal } = useLoadingModal();
 
   useEffect(() => {
     if (data) {
@@ -28,22 +28,56 @@ export default function PostDetails() {
       }
     }
     if (fetchError) {
-      setError(fetchError);
+      Store.addNotification({
+        title: "Oops!",
+        message: "An error occured while charging this post",
+        type: "danger",
+        insert: "bottom",
+        container: "bottom-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3000,
+          onScreen: true,
+        },
+      });
     }
-  }, [data, post?.content, setError, fetchError]);
+  }, [data, post?.content, fetchError]);
 
   async function saveChanges() {
     if (post !== postUpdated && postUpdated) {
       try {
-        setIsProcessing(true);
+        openModal();
         await updatePost(postUpdated);
-        setIsProcessing(false);
+        closeModal();
+        Store.addNotification({
+          title: "Success!",
+          message: "Changes saved successfully",
+          type: "success",
+          insert: "bottom",
+          container: "bottom-right",
+          animationIn: ["animate__animated", "animate__fadeIn"],
+          animationOut: ["animate__animated", "animate__fadeOut"],
+          dismiss: {
+            duration: 3000,
+            onScreen: true,
+          },
+        });
       } catch (e) {
-        setIsProcessing(false);
-        if (e instanceof Error) {
-          const message = getErrorMessage(e);
-          setError(message);
-        }
+        closeModal();
+        Store.addNotification({
+          title: "Oops!",
+          message: "An error occured while performing this action",
+          type: "danger",
+          insert: "bottom",
+          container: "bottom-right",
+          animationIn: ["animate__animated", "animate__fadeIn"],
+          animationOut: ["animate__animated", "animate__fadeOut"],
+          dismiss: {
+            duration: 3000,
+            onScreen: true,
+          },
+        });
       }
     }
   }
@@ -52,10 +86,35 @@ export default function PostDetails() {
     if (post) {
       const response = await deletePost(post.id);
       if (response.status !== 200) {
-        setError;
+        Store.addNotification({
+          title: "Oops!",
+          message: "An error occured while performing this action",
+          type: "danger",
+          insert: "bottom",
+          container: "bottom-right",
+          animationIn: ["animate__animated", "animate__fadeIn"],
+          animationOut: ["animate__animated", "animate__fadeOut"],
+          dismiss: {
+            duration: 3000,
+            onScreen: true,
+          },
+        });
         return;
       }
       navigate("/");
+      Store.addNotification({
+        title: "Success!",
+        message: "Post deleted successfully",
+        type: "success",
+        insert: "bottom",
+        container: "bottom-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3000,
+          onScreen: true,
+        },
+      });
     }
   }
 
@@ -65,9 +124,7 @@ export default function PostDetails() {
 
   return (
     <>
-      {(isLoading || isProcessing) && <IsLoading />}
-      {(!isLoading || !isProcessing) && error && <Error error={error} />}
-      {!fetchError && !isLoading && !post ? (
+      {!post ? (
         <NoDetails />
       ) : (
         !fetchError &&
@@ -97,33 +154,37 @@ export default function PostDetails() {
                 ref={textareaRef}
               />
             </div>
-            <p className="text-gray-400">
-              Posted : {post ? ConvertDateFormat(post.createdAt) : ""}
-            </p>
-            {post?.updatedAt != post?.createdAt ? (
+            <div className="mt-1">
               <p className="text-gray-400">
-                Modified : {post ? ConvertDateFormat(post.updatedAt) : ""}
+                Posted : {post ? ConvertDateFormat(post.createdAt) : ""}
               </p>
+              {post?.updatedAt != post?.createdAt ? (
+                <p className="text-gray-400">
+                  Modified : {post ? ConvertDateFormat(post.updatedAt) : ""}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+
+            {isAuthenticated() ? (
+              <div className=" flex flex-col justify-around mt-6 items-start">
+                <button
+                  className="bg-dark-200 p-2 rounded-md"
+                  onClick={saveChanges}
+                >
+                  Save changes
+                </button>
+                <button
+                  className="text-red-600 mt-2 px-2 rounded-md"
+                  onClick={removePost}
+                >
+                  Delete post
+                </button>
+              </div>
             ) : (
               ""
             )}
-
-            <div className="mt-9 flex flex-col items-start">
-              <div className="flex w-full justify-around">
-                <button
-                  className="border-2 border-[#8AEA92] text-[#8AEA92] p-[1.5px] px-2 rounded-md"
-                  onClick={saveChanges}
-                >
-                  Save
-                </button>
-                <button
-                  className="border-2 border-red-600 text-red-600 p-[1.5px] px-2 rounded-md"
-                  onClick={removePost}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
           </div>
         )
       )}
